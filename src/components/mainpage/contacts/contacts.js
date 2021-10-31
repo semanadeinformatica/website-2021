dc3df9e414e0ea0b0c4dfbcfa2fde0668cc30846
Iohan Sardinha <ioxsardinha@gmail.com>
2021-10-31 22:35:38 +0000

    sponsors

--- src/components/mainpage/contacts/contacts.js
+++ src/components/mainpage/contacts/contacts.js
@@ -8,29 +8,44 @@ import {
   FaInstagramSquare
 } from "react-icons/fa";
 
+import {
+  AiOutlineLinkedin,
+  AiOutlineFacebook,
+  AiOutlineInstagram
+} from "react-icons/ai";
+
+import { BsEnvelope } from "react-icons/bs";
+
+import { GoLocation } from "react-icons/go";
+
 import ContactStyles from "../../../styles/mainpage/contacts.module.css";
 import mainStyles from "../../../styles/mainpage/mainpage.module.css";
 
 const links = [
   {
-    icon: <FaEnvelope />,
-    href: "mailto:geral@sinf.pt",
-    text: "geral@sinf.pt"
-  },
-  {
-    icon: <FaLinkedin />,
+    icon: <AiOutlineLinkedin />,
     href: "https://www.linkedin.com/company/sinffeup/",
     text: "sinffeup"
   },
   {
-    icon: <FaFacebookSquare />,
+    icon: <BsEnvelope />,
+    href: "mailto:geral@sinf.pt",
+    text: "geral@sinf.pt"
+  },
+  {
+    icon: <AiOutlineFacebook />,
     href: "https://facebook.com/sinfFEUP",
     text: "sinffeup"
   },
   {
-    icon: <FaInstagramSquare />,
+    icon: <AiOutlineInstagram />,
     href: "https://instagram.com/sinfFEUP",
     text: "sinffeup"
+  },
+  {
+    icon: <GoLocation />,
+    href: "https://facebook.com/sinfFEUP",
+    text: "FEUP, R. Dr. Roberto Frias. 4200-465, Porto - Portugal"
   }
 ];
 
